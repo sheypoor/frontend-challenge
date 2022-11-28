4a3fcb17474805b4220fc59b010cee30ada0bb88
Danial <sdnoaein@gmail.com>
2022-11-28 14:17:28 +0330

    feat: :art: init home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-const App = () => {
-  return <>HELLO</>;
+import { Card, CardContent, Container, Typography } from "@mui/material";
+
+const App: React.FC = () => {
+  return (
+    <Container maxWidth='xs'>
+      <Card sx={{ marginTop: "64px" }}>
+        <CardContent>
+          <Typography
+            variant='h3'
+            gutterBottom
+          >
+            Sign Up
+          </Typography>
+        </CardContent>
+      </Card>
+    </Container>
+  );
 };
 
 export default App;
