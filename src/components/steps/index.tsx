7f8056b9208096b91dd7dffacd32c7557d90c78d
Danial <sdnoaein@gmail.com>
2022-11-28 14:39:17 +0330

    feat: :art: add steps container

---
+++ src/components/steps/index.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+const steps = ["Information", "Subscribtion"];
+
+export default function Steps() {
+  //   const [activeStep, setActiveStep] = useState(0);
+  const [currentStep, setCurrentStep] = useState(0);
+
+  const handleReview = () => {
+    setCurrentStep(0);
+  };
+
+  const handleSubmitInformation = () => {
+    setCurrentStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleSubscribe = () => {};
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Stepper
+        activeStep={currentStep}
+        alternativeLabel
+      >
+        {steps.map((label, index) => {
+          const stepProps: { completed?: boolean } = {};
+          const labelProps: {
+            optional?: React.ReactNode;
+          } = {};
+
+          return (
+            <Step
+              key={label}
+              {...stepProps}
+            >
+              <StepLabel {...labelProps}>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+
+      <>
+        <Typography sx={{ mt: 2, mb: 1 }}>Step {currentStep + 1}</Typography>
+        <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+          {currentStep > 0 && (
+            <Button
+              color='inherit'
+              disabled={currentStep === 0}
+              onClick={handleReview}
+              sx={{ mr: 1 }}
+            >
+              Review Info
+            </Button>
+          )}
+          <Box sx={{ flex: "1 1 auto" }} />
+
+          <Button
+            variant='contained'
+            onClick={
+              currentStep === 0 ? handleSubmitInformation : handleSubscribe
+            }
+          >
+            {currentStep === steps.length - 1 ? "Subscribe" : "Next"}
+          </Button>
+        </Box>
+      </>
+    </Box>
+  );
+}
