5ec29a0902d2ea9d3ca15544b405a22563916b00
Saleh Shojaei <ordinarysaleh@pm.me>
2022-12-03 13:37:54 +0330

    chore: init tailwind and material tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+
+module.exports = withMT({
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+})
