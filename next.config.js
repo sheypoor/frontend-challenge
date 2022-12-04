fcf9bd08e0c6a6435561251444fddcff3e0a805c
Saleh Shojaei <ordinarysaleh@pm.me>
2022-12-04 14:54:55 +0330

    Revert "feat: init ghpages"

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
-  assetPrefix: isProd ? '/frontend-challenge/' : '',
-  images: {
-    unoptimized: true,
+  experimental: {
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
