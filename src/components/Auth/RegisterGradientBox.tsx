673ed64161b14f21ce7f3181da1693ad0ce40267
Saleh Shojaei <ordinarysaleh@pm.me>
2022-12-04 14:27:27 +0330

    fix: resposive issue

--- src/components/Auth/RegisterGradientBox.tsx
+++ src/components/Auth/RegisterGradientBox.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const RegisterGradientBox = ({ stepper }) => {
   return (
     <>
-      <div className="gradient flex flex-1 items-center justify-center rounded-tl-3xl rounded-br-3xl bg-gradient-to-r from-blue-600 to-blue-400">
+      <div className="gradient hidden flex-1 items-center justify-center rounded-tl-3xl rounded-br-3xl bg-gradient-to-r from-blue-600 to-blue-400 lg:flex">
         <span className="text-8xl text-white">{stepper.icon}</span>
       </div>
     </>
