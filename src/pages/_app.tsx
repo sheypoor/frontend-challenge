467b99de3c93ae55c93dec8b19c8d9261fd61f34
Saleh Shojaei <ordinarysaleh@pm.me>
2022-12-05 00:41:19 +0330

    fix: document rtl issue in production version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,11 @@
 import type { AppProps } from 'next/app'
 import 'styles/globals.scss'
 import { ThemeProvider } from '@material-tailwind/react'
-import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <html dir="rtl" />
-      </Head>
-      <ThemeProvider>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
   )
 }
