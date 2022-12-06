2fe3e54b48e983794ece49e46a8d7fc8bdaf0ad3
Mohammad Reza Taghimomeni <30400627+mohammadreza-taghimomeni@users.noreply.github.com>
2022-12-06 10:43:11 +0330

    add component set

--- src/presentation/container/index.js
+++ src/presentation/container/index.js
@@ -1,6 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import FirstStepForm from "../view/firstStepForm";
 import SecondStepForm from "../view/secondStepForm";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./index.scss";
 
 const router = createBrowserRouter([
   {
