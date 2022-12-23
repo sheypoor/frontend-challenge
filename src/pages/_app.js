b27c076a6ab6ba9587e2dba08d36c6d1593acadf
elyas <lost_ring123@yahoo.com>
2022-12-23 14:20:50 +0330

    feat(context): added form context

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { useState, createContext } from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import { ThemeProvider } from '@mui/material/styles';
@@ -6,24 +6,24 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
 import theme from '~/MUI/theme';
 import createEmotionCache from '~/MUI/createEmotionCache';
-
-// Client-side cache, shared for the whole session of the user in the browser.
+import FormContext from '~/context/FormContext';
 const clientSideEmotionCache = createEmotionCache();
 
 export default function MyApp(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-
+  const [activeStep, setActiveStep] = useState(0);
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+    <FormContext.Provider value={{ activeStep, setActiveStep }}>
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    </FormContext.Provider>
   );
 }
 
