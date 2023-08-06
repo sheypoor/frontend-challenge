a6f01a8f21853d2d0f9cc4952118f6ceefab9224
reza <mrr.reza@gmail.com>
2023-08-07 00:41:06 +0330

    gh-pages set

--- src/componenets/Dashboard.jsx
+++ src/componenets/Dashboard.jsx
@@ -10,7 +10,8 @@ const Dashboard = () => {
     <div className="container mx-auto max-w-md mt-20 p-6 bg-white shadow-md rounded-lg">
       <h2 className="text-3xl font-semibold mb-6">Dashboard</h2>
       <p className="mb-4">
-        Welcome, User! You can add a new user by starting the wizard below:
+        Welcome! You can enter the user creation flow by clicking the button
+        below:
       </p>
       <button onClick={handleWizardStart} className="button_primary">
         Add a New User
