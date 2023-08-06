d848844b04a0dcafe950ab6d856689734abaf086
reza <mrr.reza@gmail.com>
2023-08-07 01:23:30 +0330

    resolve gh-pages issue with router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles.css";
 import ProfileInfoCapture from "./componenets/steps/ProfileInfoCapture";
 import PersonalInfoCapture from "./componenets/steps/PersonalInfoCapture";
