01317b9cce7f24ea6a3060fe851ad3417b0fb70d
arianshahalami <arian_shahalami@outlook.com>
2023-08-07 23:39:28 +0330

    feat(style-guide): reformatted whole project
    based on editorConfig and prettier   configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 function App() {
-    return "App component"
+    return "App component";
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App/>
+        <App />
     </React.StrictMode>,
-)
+);
