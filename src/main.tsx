ea14c451c23d8f3bb40bf03d6ac4192b6d99d587
arianshahalami <arian_shahalami@outlook.com>
2023-08-08 13:10:29 +0330

    feat(structure): move app component to the components folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function App() {
-    return "App component";
-}
+import { App } from "./components/app/app.component.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
