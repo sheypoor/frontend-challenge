21d3a62ab374892a3c6831a38f288704ccb9f6c1
arianshahalami <arian_shahalami@outlook.com>
2023-08-08 16:16:26 +0330

    feat(component): add card and related styles

--- src/components/introduction/introduction.component.tsx
+++ src/components/introduction/introduction.component.tsx
@@ -1,13 +1,22 @@
+import introductionSideImage from "../../assets/image/6884525.webp";
+
 import "./introduction.style.scss";
 
 export const Introcution = () => {
     return (
-        <div className="card">
-            <div style={{ marginBottom: "1rem" }}>asdasd</div>
-            <div style={{ marginBottom: "1rem" }}>asdasd</div>
-            <div style={{ marginBottom: "1rem" }}>asdasd</div>
-            <div style={{ marginBottom: "1rem" }}>asdasd</div>
-            <div style={{ marginBottom: "1rem" }}>asdasd</div>
+        <div className="card introduction-card">
+            <div className="card__container">
+                <div className="card__side-image-holder">
+                    <img
+                        className="card__side-image"
+                        alt="introduction-alt-image"
+                        src={introductionSideImage}
+                    />
+                </div>
+                <div className="card__title">
+                    <h1 className="fw-900 f-48">به پلتفرم خبررسانی شیپور خوش‌آمدید</h1>
+                </div>
+            </div>
         </div>
     );
 };
