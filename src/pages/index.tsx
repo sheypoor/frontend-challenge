55d03c4f1a68f8f73fe7bc8e54f3657e6dc704a2
Mahdi Majdian <MajdianMahdi@gmail.com>
2023-10-23 13:43:31 +0330

    feat(config): add routes for signup steps

---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+export { default as PersonalInformation } from './personal-information';
+export { default as Newsletter } from './newsletter';
+export { default as SignUpComplete } from './sign-up-complete';
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { Newsletter, PersonalInformation, SignUpComplete } from '../pages';
+
+const PERSONAL_INFORMATION_ADDRESS = 'personal-information';
 
 function RoutesComponent(): React.ReactElement {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path='/' element={<div>home</div>} />
-				<Route path='/*' element={<Navigate to={'/'} />} />
+				<Route
+					path='/'
+					element={<Navigate to={`/${PERSONAL_INFORMATION_ADDRESS}`} />}
+				/>
+				<Route
+					path={`/${PERSONAL_INFORMATION_ADDRESS}/`}
+					element={<PersonalInformation />}
+				/>
+				<Route path='/newsletter/' element={<Newsletter />} />
+				<Route path='/complete/' element={<SignUpComplete />} />
+				<Route
+					path='/*'
+					element={<Navigate to={`/${PERSONAL_INFORMATION_ADDRESS}`} />}
+				/>
 			</Routes>
 		</BrowserRouter>
 	);
