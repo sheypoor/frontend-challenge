2a5173458438a46e71ff050a4e56afc5b6ce0cde
audreybozorgi <m.bozorgi@nizek.com>
2023-10-26 03:06:01 +0330

    chor/ add necessary plugin for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), pluginRewriteAll()],
     resolve: {
         alias: {
             "src": path.resolve(__dirname, "./src/"),
-        }
-    }
+        },
+        extensions: ['.ts', '.tsx']
+    },
 })
